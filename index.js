a7844629dd36fa159dcfa1d2eced95554dc440fb
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-08 12:19:38 +0600

    Single Inventory API added

--- index.js
+++ index.js
@@ -85,6 +85,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await inventoryCollection.findOne(query)
+            res.send(result)
+        })
+
     }
     finally {
 
